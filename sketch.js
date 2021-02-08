a8e677dba2057039b7893fea11fd135157c744a5
FlipIt2018 <71611016+FlipIt2018@users.noreply.github.com>
2021-02-07 19:06:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -49,7 +49,7 @@ background("White");
 
 function keyPressed(){
 	if (keyCode===UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85})
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:15,y:-20})
 	}
 }
 
